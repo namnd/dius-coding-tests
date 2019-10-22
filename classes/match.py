0b8840cd8cda6566e88295406148a6877f8b7dcb
Nam Nguyen <namnd@nams-mbp.home>
2019-10-22 21:50:28 +1100

    Tie break & winning set

--- classes/match.py
+++ classes/match.py
@@ -3,6 +3,8 @@ class Match():
   points = {0:0, 1:15, 2:30, 3: 40}
   scores = {} # keep track of current game scores
   sets = {} # keep track of winning games
+  tie_break = False
+  winner = None
 
   def __init__(self, p1, p2):
     self.p1 = p1
@@ -12,26 +14,6 @@ class Match():
     self.__start_new_game()
 
 
-  def __str__(self):
-    scores_str = ""
-    if min(self.scores.values()) >= 3:
-      if self.scores[self.p1] == self.scores[self.p2]:
-        scores_str = ", Deuce"
-      else:
-        scores_str = ', Advantage {}'.format(self.p1 if self.scores[self.p1] > self.scores[self.p2] else self.p2)
-    elif max(self.scores.values()) > 0:
-      scores_str = ', {}-{}'.format(self.points[self.scores[self.p1]], self.points[self.scores[self.p2]])
-
-
-    sets_str = '{}-{}'.format(self.sets[self.p1], self.sets[self.p2])
-
-    return '{}{}'.format(sets_str, scores_str)
-
-
-  def __repr__(self):
-    return '{}{}'.format(type(self),self.__dict__)
-
-
   def __start_new_game(self):
     self.scores[self.p1] = 0
     self.scores[self.p2] = 0
@@ -39,13 +21,52 @@ class Match():
 
   def point_won_by(self, player):
     self.scores[player] = self.scores[player] + 1
-    if max(self.scores.values()) >= 4 and abs(self.scores[self.p1] - self.scores[self.p2]) >= 2:
+    # If tie break, player need to reach 7 points
+    threshold = 7 if self.tie_break else 4
+    if max(self.scores.values()) >= threshold and abs(self.scores[self.p1] - self.scores[self.p2]) >= 2:
       self.game_won_by(player)
 
 
   def game_won_by(self, player):
     self.sets[player] = self.sets[player] + 1
-    self.start_new_game()
+    if max(self.sets.values()) >= 6:
+      if self.tie_break and self.sets[self.p1] != self.sets[self.p2]:
+        self.winner = player
+      elif abs(self.sets[self.p1] - self.sets[self.p2]) >= 2:
+        self.winner = player
+      elif self.sets[self.p1] == self.sets[self.p2]:
+        self.tie_break = True
+
+
+    self.__start_new_game()
+
+
+  def __str__(self):
+    scores_str = ""
+    if self.tie_break:
+      if max(self.scores.values()) > 0:
+        scores_str = ', {}-{}'.format(self.scores[self.p1], self.scores[self.p2])
+    else:
+      if min(self.scores.values()) >= 3:
+        if self.scores[self.p1] == self.scores[self.p2]:
+          scores_str = ", Deuce"
+        else:
+          scores_str = ', Advantage {}'.format(self.p1 if self.scores[self.p1] > self.scores[self.p2] else self.p2)
+      elif max(self.scores.values()) > 0:
+        scores_str = ', {}-{}'.format(self.points[self.scores[self.p1]], self.points[self.scores[self.p2]])
+
+
+    sets_str = ""
+    if self.winner is None:
+      sets_str = '{}-{}'.format(self.sets[self.p1], self.sets[self.p2])
+    else:
+      sets_str = '{} is the winner'.format(self.winner)
+
+    return '{}{}'.format(sets_str, scores_str)
+
+
+  def __repr__(self):
+    return '{}{}'.format(type(self),self.__dict__)
 
 
   def score(self):
--- tests/test_match.py
+++ tests/test_match.py
@@ -38,5 +38,105 @@ class MatchTest(unittest.TestCase):
     self.assertEqual(match.__str__(), '0-1')
 
 
+  def test_point_won_by_sets_6_4(self):
+    match = Match('p1', 'p2')
+    for i in range(20):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '5-0')
+
+    for i in range(16):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '5-4')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), 'p1 is the winner')
+
+
+  def test_point_won_by_sets_7_5(self):
+    match = Match('p1', 'p2')
+    for i in range(20):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '5-0')
+
+    for i in range(20):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '5-5')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-5')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), 'p1 is the winner')
+
+
+  def test_point_won_by_tie_break(self):
+    match = Match('p1', 'p2')
+    for i in range(20):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '5-0')
+
+    for i in range(20):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '5-5')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-5')
+
+    for i in range(4):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '6-6')
+
+    # Tie break start
+    match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-6, 1-0')
+    match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '6-6, 1-1')
+    match.point_won_by('p2')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-6, 5-2')
+    # Match point
+    for i in range(5):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), 'p2 is the winner')
+
+
+  def test_point_won_by_tie_break_match_point(self):
+    match = Match('p1', 'p2')
+    for i in range(20):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '5-0')
+
+    for i in range(20):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '5-5')
+
+    for i in range(4):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-5')
+
+    for i in range(4):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '6-6')
+
+    # Tie break start
+    for i in range(6):
+      match.point_won_by('p1')
+    self.assertEqual(match.__str__(), '6-6, 6-0')
+
+    for i in range(6):
+      match.point_won_by('p2')
+    self.assertEqual(match.__str__(), '6-6, 6-6')
+
+    match.point_won_by('p1')
+    match.point_won_by('p1')
+    self.assertEqual(match.__str__(), 'p1 is the winner')
+
+
 if __name__ == '__main__':
   unittest.main()
